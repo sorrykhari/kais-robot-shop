fb152ed07c809d9481f8a561f3c3297a79df836b
Kai <kharimobley@gmail.com>
2025-06-04 00:03:15 -0400

    Files will be ignored.

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+// This is no longer a thing in the latest Angular versions, so it's just for practice purposes.
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  imports: [],
+  selector: 'bot-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
