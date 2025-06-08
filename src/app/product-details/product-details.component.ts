148aa222d36c81002443679c52235aa7c907085b
sorrykhari <kharimobley@gmail.com>
2025-06-08 09:16:01 -0400

    New branch

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,23 +1,21 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { IProduct } from '../catalog/product.model';
 import { NgIf, NgClass, CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'bot-product-details',
-  imports: [ NgIf, NgClass,CurrencyPipe ],
+  imports: [ NgIf, NgClass,CurrencyPipe, ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
 export class ProductDetailsComponent {
   @Input() product!: IProduct;
-  @Output() buy = new EventEmitter();
-  
+  cart: IProduct[] = [];
+
   getImageUrl(product: IProduct) {
       if (!product) return '';
       return '/assets/images/robot-parts/' + product.imageName;
     }
 
-  buyButtonClicked(product: IProduct) {
-    this.buy.emit();
-  }
+    addToCart(product: IProduct) {}
 }
